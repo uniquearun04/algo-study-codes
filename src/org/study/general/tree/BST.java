cf90e3d6e7b0f67bc97b31b1a8619c405cf4d71a
unknown <Arun.Chaudhary@NOD-BF1-L1-F014.INDIATIMES.CO.IN>
2014-01-25 20:59:40 +0530

    BST

--- src/org/study/general/tree/BST.java
+++ src/org/study/general/tree/BST.java
@@ -12,10 +12,45 @@ public class BST {
 			this.root = null;
 			return;
 		}
-		
+		insert(this.root,node);
 	}
 	
 	
+	public void insert(Node root, Node node){
+		Node curNode = null;
+		if(node.getVal() < root.getVal()){
+			curNode = root.getLeft();
+			if(curNode == null){
+				root.setLeft(node);
+			}
+		} else {
+			curNode = root.getLeft();
+			if(curNode == null){
+				root.setRight(node);
+			}
+		}
+		insert(curNode, node);
+	}
+	
+public boolean delete(Node node){
+	
+	if(this.root == null){
+		return false;
+	}
+	return delete(this.root, node);
+}
+	
+public boolean delete(Node root, Node node){
+	if(root == null){
+		return false;
+	}
+	Node currNode = null;
+	if(node.getVal() < root.getVal()){
+		currNode = root.getLeft(); 
+	} else if(node)
+	
+	return false;
+}
 	public Node getRoot() {
 		return root;
 	}
